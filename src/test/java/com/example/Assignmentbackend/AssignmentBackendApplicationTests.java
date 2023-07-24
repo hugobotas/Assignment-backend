a7110a33ec7088acbec034e68ee485ed6a94140b
Hugo Botas <hab12335@campus.fct.unl.pt>
2023-07-24 11:59:23 +0100

    Initial Commit

---
+++ src/test/java/com/example/Assignmentbackend/AssignmentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Assignmentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
